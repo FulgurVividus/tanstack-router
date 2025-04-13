25741ab999bb4f02039650e08a5d69f54283249b
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-13 21:44:26 +0500

    feat: custom 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createRouter, Link, RouterProvider } from "@tanstack/react-router";
 import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => (
+    <div>
+      <h1>You kidding ?? It is not found 🤣</h1>
+      <Link to="/">Go home bro</Link>
+    </div>
+  ),
+});
 
 // we need to declare and register router instance for actual type safety in TS
 declare module "@tanstack/react-router" {
