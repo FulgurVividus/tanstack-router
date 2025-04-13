ee9c4667b7be2c08f3a515c663d1591ff10f1839
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-13 21:24:02 +0500

    feat: added navigation link

--- src/routes/contact.lazy.tsx
+++ src/routes/contact.lazy.tsx
@@ -1,13 +1,18 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/contact")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Contact Page</h1>
+      <button onClick={() => navigate({ to: "/about" })}>
+        Go to About Page
+      </button>
     </div>
   );
 }
