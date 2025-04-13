45ff79cf62ec82fa37b2d650ab53bf826b5a26fc
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-13 20:44:36 +0500

    feat: created root route component

---
+++ src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <nav>
+        <Link to={"/home"}>Home</Link>
+        <Link to={"/contact"}>Contact</Link>
+        <Link to={"/about"}>About</Link>
+      </nav>
+      <Outlet />
+      <footer>Copyright ©{new Date().getFullYear()}</footer>
+    </React.Fragment>
+  );
+}
